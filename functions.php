8cdedf014bcbc5ad0f7960efe241497bc1d6ed04
abooBala <aboobala64@gmail.com>
2025-06-03 18:54:38 +0100

    Initial commit: WordPress theme setup with basic structure and assets

---
+++ functions.php
@@ -0,0 +1,22 @@
+
+<?php
+
+function style_files() {
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('style', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('style', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('js-script', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+}
+
+// Load CSS file
+add_action('wp_enqueue_scripts', 'style_files');
+
+function theme_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'theme_features');
+
+?>
