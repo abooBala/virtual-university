b32ae4f641659e25e3c58d665dca712ee270f31c
abooBala <aboobala64@gmail.com>
2025-06-06 01:11:37 +0100

    More control on inner pages

--- functions.php
+++ functions.php
@@ -23,6 +23,8 @@ add_action('after_setup_theme', 'theme_features');
 // Add Custom Post Types
 function school_post_types() {
     register_post_type('event', array(
+        'supports' => array('title', 'editor','excerpt'),
+        'show_in_rest' => true,
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
