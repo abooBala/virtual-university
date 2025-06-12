97ceb8849c2acb81354b0f8511c2e4b07e0c3798
abooBala <aboobala64@gmail.com>
2025-06-12 17:45:05 +0100

    Enforced DRY code with pageBanner function

--- single-professor.php
+++ single-professor.php
@@ -4,7 +4,12 @@ get_header();
 
 while(have_posts()) {
     the_post(); 
-    pageBanner();
+    pageBanner(array(
+        'title' => '',
+        'subtitle' => '',
+        'photo'=> '',
+
+    ));
     ?>
 
     <div class="container container--narrow page-section">
